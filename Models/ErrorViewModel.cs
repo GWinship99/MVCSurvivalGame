7c29c5d4ea71f267d15077859659930329de1a6c
Georgia Winship <georgia_winship@yahoo.com>
2025-09-29 09:44:02 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCSurvivalGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
